87b381e08e7c552d34a6ed4ca469b6eb6866fd98
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-21 15:58:19 +0300

    Proje dosyası ekle.

---
+++ FormGrafikleri3/GrafikFrm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FormGrafikleri3
+{
+    partial class GrafikFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ciz = new System.Windows.Forms.Button();
+            this.temizlebtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ciz
+            // 
+            this.ciz.Location = new System.Drawing.Point(675, 378);
+            this.ciz.Name = "ciz";
+            this.ciz.Size = new System.Drawing.Size(89, 38);
+            this.ciz.TabIndex = 0;
+            this.ciz.Text = "Çiz";
+            this.ciz.UseVisualStyleBackColor = true;
+            this.ciz.Click += new System.EventHandler(this.ciz_Click);
+            // 
+            // temizlebtn
+            // 
+            this.temizlebtn.Location = new System.Drawing.Point(675, 334);
+            this.temizlebtn.Name = "temizlebtn";
+            this.temizlebtn.Size = new System.Drawing.Size(89, 38);
+            this.temizlebtn.TabIndex = 1;
+            this.temizlebtn.Text = "Temizle";
+            this.temizlebtn.UseVisualStyleBackColor = true;
+            this.temizlebtn.Click += new System.EventHandler(this.temizlebtn_Click);
+            // 
+            // GrafikFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.temizlebtn);
+            this.Controls.Add(this.ciz);
+            this.Name = "GrafikFrm";
+            this.Text = "Form Grafikleri";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ciz;
+        private System.Windows.Forms.Button temizlebtn;
+    }
+}
+
---
+++ FormGrafikleri3/GrafikFrm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormGrafikleri3
+{
+    public partial class GrafikFrm : Form
+    {
+        Graphics g;
+        public GrafikFrm()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        private void ciz_Click(object sender, EventArgs e)
+        {
+            Random r=new Random();
+            
+            for (int i = 0; i < 100; i++)
+            {
+                int x=r.Next(100);
+                g.FillEllipse(Brushes.Blue,r.Next(500),r.Next(300),x,x);
+            }
+        }
+
+        private void temizlebtn_Click(object sender, EventArgs e)
+        {
+            g.Clear(this.BackColor);
+        }
+    }
+}
